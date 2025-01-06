e3f5ac5f83b52f5154dc066f5e28a4d6f1b010a1
LuckyEvGen <luckybird.work@gmail.com>
2025-01-06 22:39:01 +0300

    good

--- module_9_4.py
+++ module_9_4.py
@@ -12,7 +12,8 @@ class MysticBall:
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
         with open(file_name, 'w') as f:
-            f.write(str(data_set))
+            for param in data_set:
+                f.write(str(param)+'\n')
 
     return write_everything
 
